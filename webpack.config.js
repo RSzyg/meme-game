1d14d995c9e5bfbdcf3e4b4ea52e622cd1b53e74
RSzyh <750332908@qq.com>
2018-06-28 16:43:36 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/Entry.ts',
+  module: {
+    rules: [
+      {
+	test: /\.tsx?$/,
+	use: [
+	  { loader: 'ts-loader' },
+	  { loader: 'tslint-loader' }
+	]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/')
+  }
+};
