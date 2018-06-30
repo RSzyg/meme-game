7f26bf5159e095213492e8f1635da60296d2fe09
RSzyh <750332908@qq.com>
2018-06-30 21:08:15 +0800

    add role move

--- src/Main.ts
+++ src/Main.ts
@@ -1,39 +1,123 @@
 import Role from "./Role";
 
 export default class Main {
-  public images: {[key: string]: HTMLImageElement};
-  private canvasHeight: number = 600;
-  private canvasWidth: number = 800;
-  private canvas: HTMLCanvasElement;
-  private ctx: CanvasRenderingContext2D;
-  private map: number[];
-  private roles: Role[];
-  constructor() {
-    this.images = {};
-    this.canvas = document.createElement("canvas");
-    this.canvas.height = this.canvasHeight;
-    this.canvas.width = this.canvasWidth;
-    document.getElementById("display").appendChild(this.canvas);
-    this.ctx = this.canvas.getContext("2d");
-    this.map = [];
-    this.roles = [];
-  }
+    public images: { [key: string]: HTMLImageElement };
+    private canvasHeight: number = 600;
+    private canvasWidth: number = 800;
+    private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
+    private map: number[];
+    private roles: {[key: string]: Role};
+    private interval: number = 17;
+    // keyboardRecorder
+    private up: number;
+    private down: number;
+    private left: number;
+    private right: number;
+    constructor() {
+        this.images = {};
+        this.canvas = document.createElement("canvas");
+        this.canvas.height = this.canvasHeight;
+        this.canvas.width = this.canvasWidth;
+        document.getElementById("display").appendChild(this.canvas);
+        this.ctx = this.canvas.getContext("2d");
+        this.map = [];
+        this.roles = {};
+        // keyboardRecorder
+        this.up = undefined;
+        this.down = undefined;
+        this.left = undefined;
+        this.right = undefined;
+    }
 
-  public createScene() {
-    const data = {
-      width: 54,
-      height: 54,
-      x: 0,
-      y: this.canvasHeight - 54,
-      maxHealthPoint: 100,
-      attackPower: 3,
-      moveSpeed: 6,
-      jumpSpeed: 10,
-    };
-    this.roles[0] = new Role(data);
-    this.roles[0].images = this.images;
-    this.roles[0].canvas = this.canvas;
-    this.roles[0].ctx = this.ctx;
-    this.roles[0].render();
-  }
+    public createScene() {
+        this.createRole("0");
+
+        document.addEventListener("keydown", (e) => this.keyboardController(e));
+        document.addEventListener("keyup", (e) => this.keyboardController(e));
+    }
+
+    private createRole(id: string) {
+        const data = {
+            width: 54,
+            height: 54,
+            x: 0,
+            y: this.canvasHeight - 54,
+            maxHealthPoint: 100,
+            attackPower: 3,
+            moveSpeed: 4,
+            jumpSpeed: 10,
+        };
+        this.roles[id] = new Role(data);
+        this.roles[id].images = this.images;
+        this.roles[id].canvas = this.canvas;
+        this.roles[id].ctx = this.ctx;
+        this.roles[id].render();
+    }
+
+    private keyboardController(e: KeyboardEvent) {
+        if (e.type === "keydown") {
+            switch (e.keyCode) {
+                case 38:
+                    if (!this.up) {
+                        this.up = setInterval(
+                            () => this.roles["0"].move("up"),
+                            this.interval,
+                        );
+                    }
+                    break;
+                case 40:
+                    if (!this.down) {
+                        this.down = setInterval(
+                            () => this.roles["0"].move("down"),
+                            this.interval,
+                        );
+                    }
+                    break;
+                case 37:
+                    if (!this.left) {
+                        this.left = setInterval(
+                            () => this.roles["0"].move("left"),
+                            this.interval,
+                        );
+                    }
+                    break;
+                case 39:
+                    if (!this.right) {
+                        this.right = setInterval(
+                            () => this.roles["0"].move("right"),
+                            this.interval,
+                        );
+                    }
+                    break;
+                case 88:
+                    break;
+                default:
+                    break;
+            }
+        } else if (e.type === "keyup") {
+            switch (e.keyCode) {
+                case 38:
+                    clearInterval(this.up);
+                    this.up = undefined;
+                    break;
+                case 40:
+                    clearInterval(this.down);
+                    this.down = undefined;
+                    break;
+                case 37:
+                    clearInterval(this.left);
+                    this.left = undefined;
+                    break;
+                case 39:
+                    clearInterval(this.right);
+                    this.right = undefined;
+                    break;
+                case 88:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
--- src/Role.ts
+++ src/Role.ts
@@ -1,79 +1,111 @@
 export default class Role {
-  public images: {[key: string]: HTMLImageElement};
-  // canvas
-  public canvas: HTMLCanvasElement;
-  public ctx: CanvasRenderingContext2D;
-  // basic properties
-  public healthPoint: number;
-  public maxHealthPoint: number;
-  public attackPower: number;
-  public moveSpeed: number;
-  public jumpSpeed: number;
-  private selfStatus: string; // left, right, up, down and blabla(default left)
-  // element properties
-  private selfHeight: number;
-  private selfWidth: number;
-  private selfX: number;
-  private selfY: number;
-
-  constructor(data: {[key: string]: number}) {
-    // element properties
-    this.selfHeight = data.height;
-    this.selfWidth = data.width;
-    this.selfX = data.x;
-    this.selfY = data.y;
+    public images: { [key: string]: HTMLImageElement };
+    // canvas
+    public canvas: HTMLCanvasElement;
+    public ctx: CanvasRenderingContext2D;
     // basic properties
-    this.selfStatus = "left";
-    this.healthPoint = this.maxHealthPoint = data.maxHealthPoint;
-    this.attackPower = data.attackPower;
-    this.moveSpeed = data.moveSpeed;
-    this.jumpSpeed = data.jumpSpeed;
-  }
+    public healthPoint: number;
+    public maxHealthPoint: number;
+    public attackPower: number;
+    public moveSpeed: number;
+    public jumpSpeed: number;
+    private selfStatus: string; // left, right, up, down and blabla(default left)
+    // element properties
+    private selfHeight: number;
+    private selfWidth: number;
+    private selfX: number;
+    private selfY: number;
 
-  // element properties setter & getter
-  public get x(): number {
-    return this.selfX;
-  }
-  public set x(x: number) {
-    this.selfX  = x;
-  }
+    constructor(data: { [key: string]: number }) {
+        // element properties
+        this.selfHeight = data.height;
+        this.selfWidth = data.width;
+        this.selfX = data.x;
+        this.selfY = data.y;
+        // basic properties
+        this.selfStatus = "left";
+        this.healthPoint = this.maxHealthPoint = data.maxHealthPoint;
+        this.attackPower = data.attackPower;
+        this.moveSpeed = data.moveSpeed;
+        this.jumpSpeed = data.jumpSpeed;
+    }
 
-  public get y(): number {
-    return this.selfY;
-  }
-  public set y(y: number) {
-    this.selfY  = y;
-  }
+    // element properties setter & getter
+    public get x(): number {
+        return this.selfX;
+    }
+    public set x(x: number) {
+        this.selfX = x;
+    }
 
-  public get height(): number {
-    return this.selfHeight;
-  }
-  public set height(height: number) {
-    this.selfHeight  = height;
-  }
+    public get y(): number {
+        return this.selfY;
+    }
+    public set y(y: number) {
+        this.selfY = y;
+    }
 
-  public get width(): number {
-    return this.selfWidth;
-  }
-  public set width(width: number) {
-    this.selfWidth  = width;
-  }
+    public get height(): number {
+        return this.selfHeight;
+    }
+    public set height(height: number) {
+        this.selfHeight = height;
+    }
 
-  public get status(): string {
-    return this.selfStatus;
-  }
-  public set status(status: string) {
-    this.selfStatus  = status;
-  }
+    public get width(): number {
+        return this.selfWidth;
+    }
+    public set width(width: number) {
+        this.selfWidth = width;
+    }
 
-  // rendering
-  public render() {
-    this.ctx.drawImage(
-      this.images[this.selfStatus],
-      this.selfX,
-      this.selfY,
-      this.selfWidth,
-      this.selfHeight,
-    );
-  }
+    public get status(): string {
+        return this.selfStatus;
+    }
+    public set status(status: string) {
+        this.selfStatus = status;
+    }
+
+    // rendering
+    public render() {
+        this.ctx.drawImage(
+            this.images[this.selfStatus],
+            this.selfX,
+            this.selfY,
+            this.selfWidth,
+            this.selfHeight,
+        );
+    }
+
+    // remove
+    public remove() {
+        this.ctx.fillStyle = "#C0C0C0";
+        this.ctx.fillRect(
+            this.selfX,
+            this.selfY,
+            this.selfWidth,
+            this.selfHeight,
+        );
+    }
+
+    // move
+    public move(dir: string) {
+        this.remove();
+        this.status = dir;
+        switch (dir) {
+            case "up":
+                break;
+            case "down":
+                break;
+            case "left":
+                this.selfX -= this.moveSpeed;
+                break;
+            case "right":
+                this.selfX += this.moveSpeed;
+                break;
+            default:
+                break;
+        }
+        this.render();
+    }
 }
